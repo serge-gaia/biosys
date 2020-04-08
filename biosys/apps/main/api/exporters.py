97a07a55933c71466ab9ba4012e721873de36d05
Serge Le Breton <serge.lebreton@gaiaresources.com.au>
2020-04-08 08:38:28 +0800

    WIP: BIOSYS-412 BioSys export record ID for Bionet not tested missing unit tests

--- biosys/apps/main/api/exporters.py
+++ biosys/apps/main/api/exporters.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.utils import six
 from openpyxl import Workbook
 from openpyxl.styles import Font
@@ -7,27 +9,49 @@ from main.utils_data_package import GenericSchema
 
 COLUMN_HEADER_FONT = Font(bold=True)
 
+logger = logging.getLogger(__name__)
+
 
 class DefaultExporter:
-    def __init__(self, dataset, records=None):
+    """
+    dataset: Dataset
+    records: [Record]
+    mapping_funcs: defines a dictionary of functions that will be applied to the value of the field
+                   key=field.name value=func(record, field, value)
+                   Example, round latitude and longitude to 6 decimals
+                   {
+                     'latitude': lambda record, field, value: round(value, 6),
+                     'longitude': lambda record, field, value: round(value, 6),
+                   }
+    """
+
+    def __init__(self, dataset, records=None, mapping_funcs=None):
         self.ds = dataset
         self.schema = GenericSchema(dataset.schema_data)
         self.headers = self.schema.headers
         self.warnings = []
         self.errors = []
         self.records = records if records else []
+        self.mapping_funcs = mapping_funcs or {}
+        if not isinstance(self.mapping_funcs, dict):
+            raise ValueError("mapping_funcs must be a dictionary.")
 
     def row_it(self, cast=True):
         for record in self.records:
             row = []
             for field in self.schema.fields:
                 value = record.data.get(field.name, '')
-                if cast:
-                    # Cast to native python type
-                    try:
-                        value = field.cast(value)
-                    except Exception:
-                        pass
+                mapping_func = self.mapping_funcs.get(field.name)
+                # noinspection PyBroadException
+                try:
+                    if callable(mapping_func):
+                        value = mapping_func(record, field, value)
+                    else:
+                        if cast:
+                            # Cast to native python type
+                            value = field.cast(value)
+                except Exception:
+                    pass
                 # TODO: remove that when running in Python3
                 if isinstance(value, six.string_types) and not isinstance(value, six.text_type):
                     value = six.u(value)
@@ -74,7 +98,16 @@ class DefaultExporter:
 class BionetExporter(DefaultExporter):
     """
     Same as default but spit two blank lines at the top when using csv
+    07/04/2020: OEH Bionet also wants the record id in the External Key field (if any)
+    https://youtrack.gaiaresources.com.au/youtrack/issue/BIOSYS-412
     """
+    mapping_funcs = {
+        'External Key': lambda record, _field, _value: record.id
+    }
+
+    def __init__(self, dataset, records=None):
+        super().__init__(dataset, records, mapping_funcs=self.mapping_funcs)
+
     def to_csv(self, output):
         # TODO: remove when python3
         if six.PY2:
